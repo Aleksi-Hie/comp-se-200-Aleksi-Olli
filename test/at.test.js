86a15dd9f04e9e0ada235fd9ef18d77ac14d4bea
Aleksi <aleksi.hietamaki@tuni.fi>
2022-11-16 16:07:25 +0200

    temp

--- test/at.test.js
+++ test/at.test.js
@@ -1,12 +1,11 @@
 import chai from "chai"
 import at from "../src/at.js"
+const expect = chai.expect
 
 describe("At",()=>{
     it("Should pick all objects"), ()=>{
         const object = { 'a': [{ 'b': { 'c': 3 } }, 4] }
-        tested = at(object, ['a[0].b.c', 'a[1]'])
-        control = [3,4]
-        expect(tested).to.equal(control)
+        expect(at(object, ['a[0].b.c', 'a[1]'])).to.equal([3,4])
         
     }
 })
