25a450ed16a1d9b6960829f770b3f60a1af719b3
Ollin läppäri <olli.martikainen@tuni.fi>
2022-11-16 16:06:19 +0200

    fixed the test

--- test/every.test.js
+++ test/every.test.js
@@ -2,6 +2,8 @@ import chai from "chai"
 import mocha from "mocha"
 import every from "../src/every.js"
 
+const expect = chai.expect
+
 describe("Every", () => {
 	it("first test", () => {
 		expect(every([true, 1, null, 'yes'], Boolean)).to.equal(false)
