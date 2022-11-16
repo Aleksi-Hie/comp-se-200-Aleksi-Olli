ae8bc13e9ee33b618df888292cd81d8a609b04c4
Ollin läppäri <olli.martikainen@tuni.fi>
2022-11-16 16:00:21 +0200

    first test by Olli

--- test/every.test.js
+++ test/every.test.js
@@ -1,5 +1,9 @@
 import chai from "chai"
 import mocha from "mocha"
-import at from "../src/every.js"
+import every from "../src/every.js"
 
-
+describe("Every", () => {
+	it("first test", () => {
+		expect(every([true, 1, null, 'yes'], Boolean)).to.equal(false)
+	});
+})
